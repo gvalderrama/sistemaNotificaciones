a09bfa7062ed5993aa57feee514140fce35c482c
vduartev <duarte1177@gmail.com>
2013-09-12 00:30:13 -0300

    Historial

--- Notificaciones/Notificaciones/Main.cs
+++ Notificaciones/Notificaciones/Main.cs
@@ -1,5 +1,6 @@
 using System;
 using Gtk;
+using System.IO;
 
 namespace Notificaciones
 {
@@ -13,6 +14,15 @@ namespace Notificaciones
 			win.Show ();
 			Application.Run ();
 			Console.WriteLine("comprovado");
+
+			//Se crear un archivo de texto
+			StreamWriter writer = File.CreateText("./Archivo.txt");
+            writer.WriteLine("Escribo lo que quiero");
+            writer.Close();
+			Console.WriteLine("El Archivo fue Credo en la");
+			Console.WriteLine("Fecha Actual: " + DateTime.Now.ToString("yyyy/MM/dd\nHH:mm:ss"));
+			Console.WriteLine("Y");
+			Console.WriteLine("Hora Señalada: " + DateTime.Now.ToShortTimeString());
 
 		}
 	}
