06d0ac700fe835d8227bd7cd224ff8ecb3a41171
vduartev <duarte1177@gmail.com>
2013-09-24 22:43:45 -0300

    historial

--- Notificaciones/Notificaciones/Main.cs
+++ Notificaciones/Notificaciones/Main.cs
@@ -9,21 +9,15 @@ namespace Notificaciones
 		public static void Main (string[] args)
 		{
 			Application.Init ();
-			Buscador objeto = new Buscador();
+			//Buscador objeto = new Buscador();
 			MainWindow win = new MainWindow ();
 			win.Show ();
 			Application.Run ();
 			Console.WriteLine("comprovado");
 
 			//Se crear un archivo de texto
-			StreamWriter writer = File.CreateText("./Archivo.txt");
-            writer.WriteLine("Escribo lo que quiero");
-            writer.Close();
-			Console.WriteLine("El Archivo fue Credo en la");
-			Console.WriteLine("Fecha Actual: " + DateTime.Now.ToString("yyyy/MM/dd\nHH:mm:ss"));
-			Console.WriteLine("Y");
-			Console.WriteLine("Hora Señalada: " + DateTime.Now.ToShortTimeString());
-
+			Historial historial = new Historial();
+		
 		}
 	}
 }
